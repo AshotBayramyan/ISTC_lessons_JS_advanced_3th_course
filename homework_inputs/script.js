bb01c92c96c6d5de7ef65e65f9eac16f4854f144
Hasmik Poghosyan <hasmikpoghosyan@Hasmiks-MBP.Dlink>
2019-02-04 00:10:59 +0400

    in process

--- homework_inputs/script.js
+++ homework_inputs/script.js
@@ -19,20 +19,44 @@ let butt = document.createElement("button")
 butt.id = ("buttId")
 document.getElementById("inpDivId").appendChild(butt)
 butt.appendChild(document.createTextNode("Add"))
+document.getElementById("buttId").addEventListener("click", addToDoList)
+
+var err = document.getElementsByClassName('error')[0]
+var section = document.getElementsByTagName('section')[0]
+var taskList = []                                       
+var iconsClass = {                                          
+        check: 'far fa-check-square',
+        trash: 'fas fa-trash-alt'
+    }
+
+
+function addToDoList(){
+  let inp = document.getElementById("inpId")
+  if (inp.value == '') {
+    err.innerHTML = 'Please enter the task!'; 
+    return;
+}
+err.innerHTML = '';
+
+taskList.unshift( {task: inp.value, made: false} );    
+    inp.value = '';                                       
+    createTaskTable(taskList); 
+
+}
 
 // forms
 let formDiv = document.createElement("div")
 formDiv.id = "formDivId"
 document.getElementById("bodId").appendChild(formDiv)
 
-let formDiv = document.getElementById("bodId").children
+// let formDiv = document.getElementById("bodId").children
 
 
-    document.getElementById("buttId").onclick = function(){newForm()}
-    function newForm() {
+//     document.getElementById("buttId").onclick = function(){newForm()}
+//     function newForm() {
 
-        formDiv.appendChild("formDiv")
-      }
+//         formDiv.appendChild("formDiv")
+//       }
 
     //   var ul = document.getElementById("xxx")
     //   togg.appendChild(ul)
