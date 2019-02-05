8aef6d892586d0fb6d96c7e9756136afc3f948be
AshotBayramyan <ashotbayramyan@gmail.com>
2019-02-05 21:32:28 +0400

    lesson

--- loop/script.js
+++ loop/script.js
@@ -1,6 +1,10 @@
-for (let i = 0; i < 5; i++){
-    setTimeout(() => console.log(i), 10000)
-}
+// for (let i = 0; i < 5; i++){
+//     setTimeout(() => console.log(i), 0)
+// }
 
 
 // http://latentflip.com
+
+for (let i = 0; i < 10; i++){
+    setTimeout(function(){ console.log(i)}, 0)
+}
