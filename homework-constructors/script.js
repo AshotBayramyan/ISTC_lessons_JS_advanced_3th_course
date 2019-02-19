e803029bc765416e479e9aad2ebc753d4bb96cc8
AshotBayramyan <ashotbayramyan@gmail.com>
2019-02-19 17:17:45 +0400

    new homework

---
+++ homework-constructors/script.js
@@ -0,0 +1,19 @@
+document.getElementById("create").addEventListener("click", createNewPerson);
+let name = document.getElementById("first");
+let lastname = document.getElementById("last");
+let age = document.getElementById("age");
+
+function Person(name, lastname, age){
+    
+    this.firstName = name;
+    this.lastname = lastname;
+    this.age = age;
+}
+
+let newPerson = new Person(name, lastname, age)
+
+function createNewPerson(){
+
+    document.getElementById("demo").innerHTML = newPerson.firstName + " " + newPerson.lastName + " " + newPerson.age;
+}
+
