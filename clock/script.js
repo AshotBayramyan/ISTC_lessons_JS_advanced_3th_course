71853136c785124da510f1de9326ba0fb88016c1
AshotBayramyan <ashotbayramyan@gmail.com>
2019-03-14 17:27:10 +0400

    new_clock hands

--- clock/script.js
+++ clock/script.js
@@ -1,16 +1,14 @@
 var secondHand = document.getElementById("secondHand");
 var minuteHand = document.getElementById("minuteHand");
 var houreHand = document.getElementById("hourHand");
+
 function clock(){
 
-
 var date = new Date();
-
 
 var second = date.getSeconds();
 var minute = date.getMinutes();
 var hour = date.getHours();
-
 
 var secondDeg = second * 6;
 var minuteDeg = minute * 6;
